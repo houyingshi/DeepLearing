4ae28995c669faa32f355241652150f82ed80603
houyingshi@163.com <houyingshi@163.com>
2021-04-13 10:16:18 +0800

    add some keras model

--- binary_classify.py
+++ binary_classify.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import numpy as np
+from tensorflow.keras import models
+from tensorflow.keras import layers
 
 imdb = tf.keras.datasets.imdb
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
@@ -21,8 +24,30 @@ def get_text(comment_num):
     return text
 
 
+def vectorize_sequences(sequences, diamension = 10000):
+    results = np.zeros((len(sequences), diamension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1
+
+    return results
+
 
 max_word_num = get_max_word_num(train_data)
 print(max_word_num)
 comment = get_text(train_data[0])
 print(comment)
+
+x_train = vectorize_sequences(train_data)
+print(x_train[0])
+print(len(x_train[0]))
+x_test = vectorize_sequences(test_data)
+print(x_test[0])
+print(len(x_test[0]))
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
